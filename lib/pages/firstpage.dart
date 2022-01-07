eb583c2302de07905018b9d669198b168f3973bb
Nolist105 <sweetmyza@gmail.com>
2022-01-07 20:53:25 +0700

    first commit

---
+++ lib/pages/firstpage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:it_routing/pages/secondpage.dart';
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({ Key? key }) : super(key: key);
+
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('First Page',
+            style: TextStyle(fontSize: 20, color: Colors.cyan),
+            ),
+            ElevatedButton(onPressed: (){
+              var rout = MaterialPageRoute(
+                builder: (context) => SecondPage()
+              );
+              Navigator.push(context, rout);
+            }, 
+            child: const Text('ไปหน้าที่ 2'))
+          ],
+        ),),
+      
+    );
+  }
+}
